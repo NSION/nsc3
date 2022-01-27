fdc6721b53abc3f5a7104145839845f518ba1bf9
Jukka Raivio <juraivio@gmail.com>
2022-01-27 13:59:29 +0200

    Update nsc3-install.sh

--- nsc3-install.sh
+++ nsc3-install.sh
@@ -8,10 +8,27 @@ echo "++++++++++++++++++++++++++++++++++++++++"
 echo "NSC3 installation folder, e.g /home/nscuser/nsc3: "
 read  NSC3HOMEFOLDER
 export NSCHOME=$NSC3HOMEFOLDER
-echo "NSC3 public hostname, e.g videoservice.nsiontec.com: "
+echo "NSC3 public hostname, e.g videoservice.nsion.io: "
 read  NSC3URL
 export PUBLICIP=$NSC3URL
-echo "NSC3 Release tag, e.g release-3.3: " 
+echo "Location of SSL cert files, e.g /home/nscuser: "
+read  SSLF
+export SSLFOLDER=$SSLF
+# Create dictories
+mkdir $NSCHOME/logs 2> /dev/null
+mkdir $NSCHOME/mapdata 2> /dev/null
+mkdir $NSCHOME/nsc-gateway-cert 2> /dev/null
+if [ -f "$SSLFOLDER/privkey.pem" ]; then
+    cp $SSLFOLDER/privkey.pem $NSCHOME/nsc-gateway-cert/. 2> /dev/null
+else
+    echo "File "$SSLFOLDER/privkey.pem ... is missing. Move SSL cert file manaually to the folder $NSCHOME/nsc-gateway-cert/  and restart NSC3"
+fi
+if [ -f "$SSLFOLDER/fullchain.pem" ]; then
+    cp $SSLFOLDER/fullchain.pem $NSCHOME/nsc-gateway-cert/. 2> /dev/null
+else
+    echo "File "$SSLFOLDER/fullchain.pem ... is missing. Move SSL cert file manaually to the folder $NSCHOME/nsc-gateway-cert/  and restart NSC3"
+fi
+echo "NSC3 Release tag, e.g release-3.3: "
 read REL
 export NSC3REL=$REL
 export NSC3REG="registrynsion.azurecr.io"
@@ -26,50 +43,50 @@ read MAP_OPTION
 if [ $MAP_OPTION -eq 1 ]
 then
     export MAPNAME="North America"
-    echo "Selected maptile is $MAPNAME. Size 15.19 GiB"
-    echo -n "Do you want to continue downloading the map file? (y/n): "
+    echo "Selected map file is $MAPNAME. Size 15.19 GiB"
+    echo -n "Do you want to downloading the map file? (y/n): "
     read answer
     if [ "$answer" != "${answer#[Yy]}" ] ;then
-        sudo wget -k -O $NSCHOME/mapdata/north_america.mbtiles "https://nscdevstorage.blob.core.windows.net/maptiler/north-america.mbtiles?sp=ra&st=2021-04-24T14:05:55Z&se=2023-01-06T23:05:55Z&sv=2020-02-10&sr=b&sig=XGJQgOZWC6fH2zyjUEDfTQeU9e1BG67f3v4p8fVEimc%3D"
+        wget -k -O $NSCHOME/mapdata/north_america.mbtiles "https://nscdevstorage.blob.core.windows.net/maptiler/north-america.mbtiles?sp=ra&st=2021-04-24T14:05:55Z&se=2023-01-06T23:05:55Z&sv=2020-02-10&sr=b&sig=XGJQgOZWC6fH2zyjUEDfTQeU9e1BG67f3v4p8fVEimc%3D"
     else
-        echo "Continue installation without a maptile"
-    fi     
+        echo "Continue installation without maptiles"
+    fi
 fi
 if [ $MAP_OPTION -eq 2 ]
 then
     export MAPNAME="Europe"
-    echo "Selected maptile is $MAPNAME. Size 19.39 GiB"
-    echo -n "Do you want to continue downloading the map file? (y/n): "
+    echo "Selected map file is $MAPNAME. Size 19.39 GiB"
+    echo -n "Do you want to downloading the map file?? (y/n): "
     read answer
     if [ "$answer" != "${answer#[Yy]}" ] ;then
-        sudo wget -k -O $NSCHOME/mapdata/europe.mbtiles "https://nscdevstorage.blob.core.windows.net/maptiler/europe.mbtiles?sp=ra&st=2021-04-24T12:40:49Z&se=2023-01-07T21:40:49Z&sv=2020-02-10&sr=b&sig=m3ANK5J8%2BqYh80OdON0BRVCIll4ptM0%2F2kSjzGMmGrc%3D"
+        wget -k -O $NSCHOME/mapdata/europe.mbtiles "https://nscdevstorage.blob.core.windows.net/maptiler/europe.mbtiles?sp=ra&st=2021-04-24T12:40:49Z&se=2023-01-07T21:40:49Z&sv=2020-02-10&sr=b&sig=m3ANK5J8%2BqYh80OdON0BRVCIll4ptM0%2F2kSjzGMmGrc%3D"
     else
-        echo "Continue installation without a maptile"
-    fi    
+        echo "Continue installation without maptiles"
+    fi
 fi
 if [ $MAP_OPTION -eq 3 ]
 then
     export MAPNAME="Australia"
-    echo "Selected maptile is $MAPNAME. Size 1.21 GiB"
-    echo -n "Do you want to continue downloading the map file? (y/n): "
+    echo "Selected map file is $MAPNAME. Size 1.21 GiB"
+    echo -n "Do you want to downloading the map file? (y/n): "
     read answer
     if [ "$answer" != "${answer#[Yy]}" ] ;then
-        sudo wget -k -O $NSCHOME/mapdata/australia.mbtiles "https://nscdevstorage.blob.core.windows.net/maptiler/australia-oceania_australia.mbtiles?sp=ra&st=2021-04-24T14:25:30Z&se=2023-01-06T23:25:30Z&sv=2020-02-10&sr=b&sig=yWkB7loHb02SNgCIhWiNZMb8VpfppfISaJW4MnsFHMw%3D";
+        wget -k -O $NSCHOME/mapdata/australia.mbtiles "https://nscdevstorage.blob.core.windows.net/maptiler/australia-oceania_australia.mbtiles?sp=ra&st=2021-04-24T14:25:30Z&se=2023-01-06T23:25:30Z&sv=2020-02-10&sr=b&sig=yWkB7loHb02SNgCIhWiNZMb8VpfppfISaJW4MnsFHMw%3D";
     else
-        echo "Continue installation without a maptile"
-    fi    
+        echo "Continue installation without maptiles"
+    fi
 fi
 if [ $MAP_OPTION -eq 4 ]
 then
     export MAPNAME="GCC states"
-    echo "Selected maptile is $MAPNAME. Size 390.52 MiB"
-    echo -n "Do you want to continue downloading the map file? (y/n): "
+    echo "Selected map file is $MAPNAME. Size 390.52 MiB"
+    echo -n "Do you want to downloading the map file? (y/n): "
     read answer
     if [ "$answer" != "${answer#[Yy]}" ] ;then
-        sudo wget -k -O $NSCHOME/mapdata/asia.mbtiles "https://nscdevstorage.blob.core.windows.net/maptiler/asia_gcc-states.mbtiles?sp=ra&st=2021-04-25T15:03:13Z&se=2023-01-07T00:03:13Z&sv=2020-02-10&sr=b&sig=nvfmJOdn4XG2BV2CEncNXQuEnscJ2lfmtJeQZellHwM%3D"
+        wget -k -O $NSCHOME/mapdata/asia.mbtiles "https://nscdevstorage.blob.core.windows.net/maptiler/asia_gcc-states.mbtiles?sp=ra&st=2021-04-25T15:03:13Z&se=2023-01-07T00:03:13Z&sv=2020-02-10&sr=b&sig=nvfmJOdn4XG2BV2CEncNXQuEnscJ2lfmtJeQZellHwM%3D"
     else
-        echo "Continue installation without a maptile"
-    fi    
+        echo "Continue installation without maptiles"
+    fi
 fi
 if [ $MAP_OPTION -gt 4 ]
 then
@@ -81,18 +98,14 @@ then
     echo "Selected value $MAP_OPTION  is out of range 1-4!"
 exit 0
 fi
-echo "$MAPNAME map files is downloaded"
+echo "$MAPNAME map file is downloaded"
 # Move old files
 mv docker-compose.yml docker-compose-$NSC3REL.old 2> /dev/null
 # Create env file
 echo "export PUBLICIP=$PUBLICIP" > $NSCHOME/nsc-host.env
 echo "export NSCHOME=$NSCHOME" >> $NSCHOME/nsc-host.env
-# Create dictories
-sudo mkdir $NSCHOME/logs 2> /dev/null
-sudo mkdir $NSCHOME/mapdata 2> /dev/null
-sudo mkdir $NSCHOME/nsc-gateway-cert 2> /dev/null
-sudo cp $HOME/privkey.pem $NSCHOME/nsc-gateway-cert/. 2> /dev/null
-sudo cp $HOME/fullchain.pem $NSCHOME/nsc-gateway-cert/. 2> /dev/null
+#  Modify maptiles rights level
+chmod 644 $NSCHOME/mapdata/*.* 2> /dev/null
 # Create docker-compose.yml file
 cd $NSCHOME
 (echo "cat <<EOF >docker-compose-temp.yml";
@@ -101,13 +114,18 @@ cat nsc3-docker-compose-ext-reg.tmpl | sed -n '/'"$NSC3REL"'/,/'"$NSC3REL"'/p';
 . temp.yml 2> /dev/null
 cat docker-compose-temp.yml > docker-compose.yml;
 rm -f temp.yml docker-compose-temp.yml 2> /dev/null
+# Archive env specific file to system
+if test -f docker-compose_$PUBLICIP.yml; then
+    mv docker-compose_$PUBLICIP.yml docker-compose_$PUBLICIP.old  2> /dev/null
+fi
+cp docker-compose.yml docker-compose_$PUBLICIP.yml
 echo "docker-compose.yml file is created..."
 echo "Downloading docker images ..."
 sudo docker-compose up -d
 echo "++++++++++++++++++++++++++++++++++++++++"
 echo ""                                        
-echo "NSC3 backend is installed!"  
-echo ""  
+echo "NSC3 backend is installed!"
+echo ""
 echo "Login to your NSC3 web app by URL address"
 echo "https://$PUBLICIP"
 echo ""
