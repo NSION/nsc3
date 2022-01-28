d016aa97125f8b0a0a433a99ceed220be5c305eb
Jukka Raivio <juraivio@gmail.com>
2022-01-28 08:09:41 +0200

    Update valor-upgrade.sh

--- valor-upgrade.sh
+++ valor-upgrade.sh
@@ -48,7 +48,7 @@ if [ -f "docker-compose-valor.yml" ]; then
    mv docker-compose-valor.yml docker-compose-valor-$NSC3REL.old 2> /dev/null
 fi
 (echo "cat <<EOF >docker-compose-valor-temp.yml";
-cat nsc3-docker-compose-valor-ext-reg.tmpl | sed -n '/'"$NSC3REL"'/,/'"$NSC3REL"'/p';
+cat valor-docker-compose-ext-reg.tmpl | sed -n '/'"$NSC3REL"'/,/'"$NSC3REL"'/p';
 ) >temp.yml
 . temp.yml 2> /dev/null
 cat docker-compose-valor-temp.yml > docker-compose-valor.yml;
