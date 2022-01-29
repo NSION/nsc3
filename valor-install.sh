36532784ddaf79ea4a765cb7f683cc8b28e6360c
Jukka Raivio <juraivio@gmail.com>
2022-01-29 11:46:46 +0200

    Update valor-install.sh

--- valor-install.sh
+++ valor-install.sh
@@ -49,6 +49,13 @@ if [ "$silentmode" = false ]; then
     export NSC3REL=$REL
 fi
 echo "export REDISAI_DEVICE=$REDISAI_DEVICE" >> $NSCHOME/nsc-host.env
+if grep -q $NSC3REL $NSCHOME/nsc3-docker-compose-ext-reg.tmpl; then     
+   echo "$NSC3REL tag found from docker-compose template" 
+   RELEASETAG=$NSC3REL
+   else    
+   echo "Release tag: $NSC3REL is missing. Using release tag: latest as runtime parameters configuration" 
+   RELEASETAG="latest"
+fi
 # Move old files
 if [ -f "docker-compose-valor.yml" ]; then
    mv docker-compose-valor.yml docker-compose-valor-$NSC3REL.old 2> /dev/null
