1391031707ab8c3dd3d46233cc8f3f2cf5ef36e7
Jukka Raivio <juraivio@gmail.com>
2023-06-22 15:35:40 +0300

    Update nsc3-upgrade.sh

--- nsc3-upgrade.sh
+++ nsc3-upgrade.sh
@@ -2,8 +2,8 @@
 ## NSC3 registry:
 export NSC3REG="registrynsion.azurecr.io"
 source ./nsc-host.env
-export EXTIP='"'$(host $PUBLICIP | awk '{print $4}')'"' 2> /dev/null
-export MINIOSECRET=$(sudo docker inspect nsc-minio | grep MINIO_ROOT_PASSWORD= | awk '{print $1}' | sed s/MINIO_ROOT_PASSWORD=// ) 2> /dev/null
+export EXTIP=$(host $PUBLICIP | awk '{print $4}') 2> /dev/null
+export MINIOSECRET=$(sudo docker inspect nsc-minio | grep MINIO_ROOT_PASSWORD= | awk '{print $1}' | sed s/MINIO_ROOT_PASSWORD=// | sed -e 's/[""]//g') 2> /dev/null
 silentmode=false
 if [ ${1+"true"} ]; then
    if  [ $1 == "--silent" ]; then
