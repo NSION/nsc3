a9ef245be7a8b591e144ba05b407568bc76af7a2
Jukka Raivio <juraivio@gmail.com>
2023-09-15 10:38:47 +0300

    Update team-bridge-client-install.sh

--- team-bridge-client-install.sh
+++ team-bridge-client-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ## NSC3 registry:
 export NSC3REG="registrynsion.azurecr.io"
-export MINIOSECRET=$(sudo docker inspect nsc-minio | grep MINIO_ROOT_PASSWORD= | awk '{print $1}' | sed s/MINIO_ROOT_PASSWORD=// | sed -e 's/[""]//g') 2> /dev/null
 silentmode=false
 if [ ${1+"true"} ]; then
    if  [ $1 == "--silent" ]; then
