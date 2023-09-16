a23a4ab00b5b32fd06d4d0cd23000dca5f573f2b
Jukka Raivio <juraivio@gmail.com>
2023-09-16 14:51:40 +0300

    Update team-bridge-install.sh

--- team-bridge-install.sh
+++ team-bridge-install.sh
@@ -306,9 +306,7 @@ echo ""
 echo "Backup-files:"
 echo "Docker-compose backup file: 'docker-compose-tb-addition-backup-$TIMESTAMP.yml' "
 echo ""
-if [ $KEY_COPY_REMINDER ]; then echo "New TCP keypairs generated at this server. 
-Please copy all key pair files from this server folder $NSCHOME/bridgekeys 
-to other end server folder $NSCHOME/bridgekeys"; fi
+if [ $KEY_COPY_REMINDER ]; then echo "New TCP keypairs generated at this server. Please copy all key pair files from this server folder $NSCHOME/bridgekeys to other end server folder $NSCHOME/bridgekeys"; fi
 echo ""
 echo "Login to your NSC3 web app by URL address       "
 echo "https://$PUBLICIP                               "
